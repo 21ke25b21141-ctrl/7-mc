54a0a16a0d152163f1ed4eb5cc05183c68dd5b9a
21ke25b21141-ctrl <21ke25b21141@21kschool.school>
2026-02-11 10:15:17 +0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
-radio.onReceivedString(function (receivedString) {
-    basic.showString("Hello!")
+input.onButtonPressed(Button.A, function () {
+    reading = input.lightLevel()
+    led.plotBarGraph(
+    input.lightLevel(),
+    255
+    )
+    basic.pause(100)
 })
-radio.setGroup(1)
-radio.sendString("Hello")
+let reading = 0
+basic.showIcon(IconNames.Diamond)
+reading = input.lightLevel()
+basic.pause(100)
