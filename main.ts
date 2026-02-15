8a7e373b567fefabd7b7509cdb5a59a4b576981c
21ke25b21141-ctrl <21ke25b21141@21kschool.school>
2026-02-15 19:03:18 +0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,2 @@
-input.onButtonPressed(Button.A, function () {
-    reading = input.lightLevel()
-    led.plotBarGraph(
-    input.lightLevel(),
-    255
-    )
-    basic.pause(100)
-})
-let reading = 0
-basic.showIcon(IconNames.Diamond)
-reading = input.lightLevel()
-basic.pause(100)
+radio.setGroup(1)
+radio.sendString("Fuck")
